49ec34f32a87be33032df7de00360da197023355
Jeff Hodges <jeff@somethingsimilar.com>
2009-07-30 07:33:12 -0700

    font is back. just cannot use daemon

--- jmhodges/font_config.el
+++ jmhodges/font_config.el
@@ -1,3 +1,3 @@
 (provide 'font_config)
 
-; (set-face-font 'default "-apple-inconsolata-medium-r-normal--15-0-72-72-m-0-iso10646-1")
+(set-face-font 'default "-apple-inconsolata-medium-r-normal--15-0-72-72-m-0-iso10646-1")
