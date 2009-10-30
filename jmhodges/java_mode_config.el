0d1970ef86f51458fcaae9f02aea40b1ecd7bed4
Jeff Hodges <jeff@somethingsimilar.com>
2009-10-29 23:31:44 -0700

    Java indentation, gathered up for the cassandra project.

--- jmhodges/java_mode_config.el
+++ jmhodges/java_mode_config.el
@@ -2,6 +2,19 @@
 (defun jmhodges-java-identation ()
   (setq indent-tabs-mode nil)
   (setq tab-width 2)
+  (c-add-style "mine" '("java"
+                            (c-basic-offset . 4)
+                            (c-hanging-braces-alist
+                             ((substatement-open)
+                              (block-close . c-snug-do-while)
+                              (extern-lang-open after)
+                              (inexpr-class-open after)
+                              (inexpr-class-close before)))
+                            (c-offsets-alist
+                             (substatement-open . 0))
+                             ))
+  (c-set-style "mine")
+
   )
 (add-hook 'java-mode-hook 'run-coding-hook)
 (add-hook 'java-mode-hook 'jmhodges-java-identation)
