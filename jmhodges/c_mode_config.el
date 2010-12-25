fe5077fda85cfa11a77ea983f51b7f7162f75ff3
Jeff Hodges <jeff@somethingsimilar.com>
2010-12-24 23:35:07 -0800

    adding the nginx style of c

--- jmhodges/c_mode_config.el
+++ jmhodges/c_mode_config.el
@@ -1,2 +1,24 @@
-(provide 'c_mode_config)
+
+(defun nginx-c-style ()
+  (setq tab-width 4)
+  (c-set-style "nginx")
+  )
+
+(c-add-style "nginx" '( ; k&r but c-basic-offset of 4
+                       (c-basic-offset . 4)
+                       (c-comment-only-line-offset . 0)
+                       (c-offsets-alist
+                        (statement-block-intro . +)
+                        (knr-argdecl-intro . 0)
+                        (substatement-open . 0)
+                        (substatement-label . 0)
+                        (label . 0)
+                        (statement-cont . +)
+                        )
+                       )
+             )
+
 (add-hook 'c-mode-common-hook 'run-coding-hook)
+
+
+(provide 'c_mode_config)
